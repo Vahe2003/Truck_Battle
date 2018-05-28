731ab098aeb6ab30891e361ea6173388433e6c76
Vahe2003 <vahe.khachatryan.y@tumo.org>
2018-05-28 13:02:56 +0400

    chat delete

--- script.js
+++ script.js
@@ -64,39 +64,6 @@ function preload() {
     energyImage = loadImage('./Resources/power.png');
 
 }
-function main() {
-    var socket = io.connect('http://localhost:3000');
-    var chatDiv = document.getElementById('text');
-    var chatText = document.getElementsByTagName('p');
-    var input = document.getElementById('message');
-    var button = document.getElementById('submit');
-    var buttonDelete = document.getElementById('delete'); 
-
-    function handleSubmit(evt) {
-        var val = input.value;
-        if (val != "") {
-            socket.emit("send message", val);
-        }
-    }
-    button.onclick = handleSubmit;
-
-    function handleMessage(msg) {
-        var p = document.createElement('p');
-        p.innerText = msg;
-        chatDiv.appendChild(p);
-        input.value = "";
-        console.log(p)
-    }
-
-    socket.on('display message', handleMessage);
-
-    function handleDelete() {
-        chatDiv.innerHTML = '';
-    }
-    buttonDelete.onclick = handleDelete;
-} 
-window.onload = main;
-
 
 function setup() {
     createCanvas(side * 32, side * 30);
